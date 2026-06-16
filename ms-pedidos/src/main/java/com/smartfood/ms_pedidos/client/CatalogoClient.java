19c9d00cd81d886c9be30250a407fe9b36d0fae1
Benja-iv <be.illezca@duocuc.cl>
2026-06-16 17:52:58 -0400

    Modificacion de dependencias, Se agrego base de datos MySQL y h2 a todos los microservicios

--- ms-pedidos/src/main/java/com/smartfood/ms_pedidos/client/CatalogoClient.java
+++ ms-pedidos/src/main/java/com/smartfood/ms_pedidos/client/CatalogoClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.smartfood.ms_pedidos.dto.CategoriaResponseDTO;
 
-@FeignClient(name = "ms-catalogo", url = "http://localhost:8080")
+@FeignClient(name = "ms-catalogo", url = "http://localhost:8082")
 public interface CatalogoClient {
     @GetMapping("/api/categorias/{id}")
     CategoriaResponseDTO obtenerCategoriaPorId(@PathVariable("id") Long id);
--- ms-pedidos/src/main/java/com/smartfood/ms_pedidos/client/InventarioClient.java
+++ ms-pedidos/src/main/java/com/smartfood/ms_pedidos/client/InventarioClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-inventario", url = "http://localhost:8081")
+@FeignClient(name = "ms-inventario", url = "http://localhost:8085")
 public interface InventarioClient {
     @GetMapping("/api/inventario/{id}")
     String obtenerStockPorId(@PathVariable("id") Long id);
